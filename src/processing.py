862df9dd3e08900efa02fb2169d66130922c5842
Anton Egorov <mr.egorov.2001@mail.ru>
2025-06-13 17:17:59 +0400

    Created the sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,13 @@ def filter_by_state(list_of_dictionaries: list[dict], state: str = "EXECUTED") -
             if separate_dictionary["state"] == state:
                 dictionaries_with_key.append(separate_dictionary)
     return dictionaries_with_key
+
+
+def sort_by_date(list_of_dictionaries: list[dict], sort_order: bool = True) -> list:
+    """Функция, которая принимает список словарей и параметр сортировки опционально,
+    по умолчанию на убывание, возвращает список словарей отсортированных по дате"""
+    return sorted(
+        list_of_dictionaries,
+        key=lambda dictionary: dictionary["date"],
+        reverse=sort_order,
+    )
